97b7cbe65f81a4bb38e9e5278ff3d36a5c76b1b0
SHIV.KUM <SHIV.KUM@LP1-AP-51708055.PROD.HCLPNP.COM>
2020-09-08 19:45:34 +0530

    Second Commit

--- src/main/java/com/learning/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/learning/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.learning.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test new
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
